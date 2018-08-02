e58a19112223d4b81dbc6858abea1d9dad3264b3
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-02 16:00:42 +0200

    Add clone example

---
+++ clone/clone.php
@@ -0,0 +1,16 @@
+<?php
+final class Foo
+{
+    public $bar;
+}
+
+$foo = new Foo;
+$foo->bar = 12;
+$foo1 = $foo;
+$foo1->bar = 13;
+echo '$foo->bar = ' . $foo->bar . '<br>$foo1->bar = ' . $foo1->bar;
+
+$foo2 = clone $foo;
+$foo2->bar = 14;
+echo '$foo->bar = ' . $foo->bar . '<br>$foo1->bar = ' . $foo1->bar . '<br>$foo2->bar = ' . $foo2->bar;
+?>
