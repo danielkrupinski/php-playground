b13c636c35f1d3f59567a4ee05f802a6df1b28ec
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-03 15:56:55 +0200

    Preserve output formatting with <pre> tag

--- clone/clone.php
+++ clone/clone.php
@@ -8,9 +8,10 @@ $foo = new Foo;
 $foo->bar = 12;
 $foo1 = $foo;
 $foo1->bar = 13;
-echo "foo->bar = $foo->bar foo1->bar = $foo1->bar<br>";
+echo "<pre>";
+echo "foo->bar = $foo->bar    foo1->bar = $foo1->bar\n";
 
 $foo2 = clone $foo;
 $foo2->bar = 14;
-echo "foo->bar = $foo->bar foo1->bar = $foo1->bar foo2->bar = $foo2->bar";
+echo "foo->bar = $foo->bar    foo1->bar = $foo1->bar    foo2->bar = $foo2->bar";
 ?>
