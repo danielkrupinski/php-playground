f0ac2f2c70e31cc087aa9afa01597d96b2011b24
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-03 15:59:15 +0200

    Update clone.php

--- clone/clone.php
+++ clone/clone.php
@@ -6,9 +6,11 @@ final class Foo
 
 $foo = new Foo;
 $foo->bar = 12;
+echo "<pre>";
+echo "foo->bar = $foo->bar\n";
+
 $foo1 = $foo;
 $foo1->bar = 13;
-echo "<pre>";
 echo "foo->bar = $foo->bar    foo1->bar = $foo1->bar\n";
 
 $foo2 = clone $foo;
