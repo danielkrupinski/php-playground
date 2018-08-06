3a7d2ebe6292b075a66e83498ca551626d97a16e
Daniel Krupiński <danielkrupinski@outlook.com>
2018-08-06 18:20:51 +0200

    Add shuffle example

--- shuffle/shuffle.php
+++ shuffle/shuffle.php
@@ -1,3 +1,8 @@
 <?php
-
+$array = array('php', 'c++', 'fortran', 'java', 'javascript', 'python', 'c#', 'c');
+shuffle($array);
+print_r($array);
+echo "<pre><br></pre>";
+shuffle($array);
+print_r($array);
 ?>
